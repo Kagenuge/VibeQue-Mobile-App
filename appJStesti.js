79b4bc31acc730209c70281da112c9ea7183e179
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-12-11 17:23:40 +0200

    firstFetchWorking

---
+++ appJStesti.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, ScrollView } from 'react-native';
+// import Songs from './components/Songs';
+
+// export default function App() {
+//   return (
+//     <ScrollView style={(styles.container, { flex: 0 })}>
+//       <Songs />
+//     </ScrollView>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// });
