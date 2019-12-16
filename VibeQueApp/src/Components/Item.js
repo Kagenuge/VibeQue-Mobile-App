82a39ee2e9196c71c1d9e1daa23a0eed08e28baa
Kagenuge <kaarlen@gmail.com>
2019-12-16 16:05:02 +0200

    fixes...

--- VibeQueApp/src/Components/Item.js
+++ VibeQueApp/src/Components/Item.js
@@ -41,6 +41,15 @@ const styles = StyleSheet.create({
 export default ({ item: { imageUri, title, type, name, previewUrl } }) => (
   <TouchableOpacity
     onPress={() => {
+      handleClick = songUrl => {
+        console.log('url: ' + songUrl);
+        postSong(songUrl).then(res => {
+          console.log('Server response: ' + res)
+        });
+
+        // addSong(songUrl);
+      };
+
       Alert.alert(
         'Set you entrance song to be: ',
         '' + name + ' - ' + title + '',
@@ -53,7 +62,11 @@ export default ({ item: { imageUri, title, type, name, previewUrl } }) => (
           {
             text: 'OK',
             onPress: () => {
-              handleClick(previewUrl);
+              if (previewUrl) {
+                handleClick(previewUrl);
+              } else {
+                console.log('No url yet')
+              }
             }
           }
         ]
