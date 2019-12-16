3650dc14100298bc9b3d2abc936291d03bdd96d4
Meri Kojonen <merikojonen@gmail.com>
2019-12-16 16:46:44 +0200

    Home-screen fiksausta.

--- VibeQueApp/App.js
+++ VibeQueApp/App.js
@@ -4,30 +4,16 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 // import AppNavigator from './AppNavigator';
 import SongBox from './src/Components/SongBox';
-import BackgroundImage from './src/Components/BackgroundImage';
 
 class HomeScreen extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <BackgroundImage />
-=======
-=======
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <BackgroundImage />
->>>>>>> 5cb477a1306d5d768604702d1c039f91e5412eae
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black'}}>
                 <Image
           style={{width: 350, height: 80}}
           source={require('./src/pics/logo1.png')}
         />
         <SongBox />
-<<<<<<< HEAD
->>>>>>> 8c28ddbe545b4a97f98e1b825ca1bf823c314209
-=======
->>>>>>> 5cb477a1306d5d768604702d1c039f91e5412eae
       </View>
       // <View style={styles.container}>
       //   <Text>Welcome to VibeQue!</Text>
--- VibeQueApp/src/Components/BackgroundImage.js
+++ VibeQueApp/src/Components/BackgroundImage.js
@@ -1,61 +1,21 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Image,
-  View,
-  Text,
-} from 'react-native';
+import React, { Component } from 'react'
+import { Image } from 'react-native'
 
-const remote = 'https://c.wallhere.com/photos/45/55/1600x900_px_abstract_Colorful_smoke-783876.jpg!d';
-
-export default class BackgroundImage extends Component {
+class BackgroundImage extends Component {
   render() {
-    const resizeMode = 'center';
-    const text = 'I am some centered text';
-
+    const {source, children, style, ...props} = this.props
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#eee',
-        }}
-      >
-        <View
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <Image
-            style={{
-              flex: 1,
-              resizeMode,
-            }}
-            source={{ uri: remote }}
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            justifyContent: 'center',
-          }}
-        >
-          <Text
-            style={{
-              textAlign: 'center',
-              fontSize: 40,
-            }}
-          >
-            {text}
-          </Text>
-        </View>
-      </View>
-    );
+      <Image source={ source }
+             style={ { flex: 1, width: null, height: null, ...style } }
+             {...props}>
+        { children }
+      </Image>
+    )
   }
 }
-
-AppRegistry.registerComponent('BackgroundImage', () => BackgroundImage);
+BackgroundImage.propTypes = {
+  source: React.PropTypes.object,
+  children: React.PropTypes.object,
+  style: React.PropTypes.object
+}
+export default BackgroundImage
