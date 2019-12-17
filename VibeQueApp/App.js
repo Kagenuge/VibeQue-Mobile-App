5860e9c89468dd2d7b2646c25c60145ab9b3ec35
Meri Kojonen <merikojonen@gmail.com>
2019-12-17 11:17:13 +0200

    Logo uusiksi.

--- VibeQueApp/App.js
+++ VibeQueApp/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-// import AppNavigator from './AppNavigator';
 import SongBox from './src/Components/SongBox';
+//import Index from './src/Components/Index';
 
 class HomeScreen extends React.Component {
   render() {
     return (
+      // <View>
+      //   <Index />
+      // </View>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'black'}}>
                 <Image
           style={{width: 350, height: 80, marginTop: 100}}
-          source={require('./src/pics/logo1.png')}
+          source={require('./src/assets/images/logo1')}
         />
         <SongBox />
       </View>
--- VibeQueApp/src/Components/Index.js
+++ VibeQueApp/src/Components/Index.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 
-export default () => (
-  <View
+var styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+  }
+});
+class Index extends React.Component {
+  render() {
+    return (
+      <Image source={require('./images/dark_bg.jpg')} style={styles.backgroundImage}>
+        <View>
+          <Text>Welcome.</Text>
+        </View>
+      </Image>
+    );
+  }
+}
 
-  />
-);
+export default Index;
